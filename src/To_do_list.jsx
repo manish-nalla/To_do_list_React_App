446f7b3505bf99434fe4def439a7c48717eddc5b
Manish <19luckyreddy@gmail.com>
2024-11-15 12:24:27 -0500

    swap method changed

--- src/To_do_list.jsx
+++ src/To_do_list.jsx
@@ -24,10 +24,11 @@ function To_do_list() {
 
     function moveUp(index) {
         if (index > 0) {
-            const updatedTasks = [...tasks]
-            const temp = updatedTasks[index]
-            updatedTasks[index] = updatedTasks[index - 1]
-            updatedTasks[index - 1] = temp
+            let updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            // const temp = updatedTasks[index]
+            // updatedTasks[index] = updatedTasks[index - 1]
+            // updatedTasks[index - 1] = temp
             setTasks(updatedTasks)
         }
     }
@@ -35,6 +36,7 @@ function To_do_list() {
     function moveDown(index) {
         if (index < tasks.length - 1) {
             const updatedTasks = [...tasks]
+            // [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
             const temp = updatedTasks[index]
             updatedTasks[index] = updatedTasks[index + 1]
             updatedTasks[index + 1] = temp
